ab11fef6cccea2457d87fe26765880adbd7ccd87
Sudarev <soul7341@gmail.com>
2023-10-30 20:29:51 +0300

    Created XML-JSON parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,21 @@ package org.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -14,20 +25,59 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = ParserCSV.parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json, "data.json");
+        String jsonCSV = listToJson(list);
+        writeString(jsonCSV, "data.json");
+
+        List<Employee> employeeList = parserXML("data.xml");
+        String jsonXML = listToJson(employeeList);
+        writeString(jsonXML, "data2.json");
+
+
     }
 
-    public static String listToJson(List<Employee> list){
+    public static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
         return gson.toJson(list, listType);
     }
+
     public static void writeString(String json, String fileName) {
         try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Employee> parserXML(String fileName) {
+        DocumentBuilderFactory documentBuilderFactory = new DocumentBuilderFactoryImpl();
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(new File(fileName));
+            Node root = document.getDocumentElement();
+            NodeList nodes = root.getChildNodes();
+            List<Employee> list = new ArrayList<>(nodes.getLength());
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) nodes.item(i);
+                    int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    list.add(employee);
+                }
+            }
+            return list;
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
             throw new RuntimeException(e);
         }
     }
